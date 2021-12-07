8152cfb75b09cf0fb04e91cfd5683b995e6485ec
Katarina <katarina.mursec@gmail.com>
2021-12-07 12:11:35 +0100

    2021 - day 5

---
+++ 2021/05/part_1.py
@@ -0,0 +1,48 @@
+data_file = open('data.txt', 'r')
+hydrothermal_vent_lines = [[[int(position.split(',')[0]), int(position.split(
+    ',')[1])] for position in line.strip().split(' -> ')] for line in data_file.readlines()]
+
+hydrothermal_vents_positions = []
+
+
+def get_line_vent_positions(line):
+    positions = []
+    x_fixed = line[0][0] == line[1][0]
+    y_fixed = line[0][1] == line[1][1]
+    decreasing = (x_fixed and line[0][1] > line[1][1]) or (
+        y_fixed and line[0][0] > line[1][0])
+    pos_range = None
+
+    if x_fixed:
+        range_end = line[1][1] + (-1 if decreasing else 1)
+        pos_range = range(line[0][1], range_end, -1 if decreasing else 1)
+    if y_fixed:
+        range_end = line[1][0] + (-1 if decreasing else 1)
+        pos_range = range(line[0][0], range_end, -1 if decreasing else 1)
+    if not pos_range:
+        return positions
+
+    for pos in pos_range:
+        if x_fixed:
+            new_position = f'{line[0][0]},{pos}'
+        if y_fixed:
+            new_position = f'{pos},{line[0][1]}'
+        positions.append(new_position)
+    return positions
+
+
+hydrothermal_vents_positions_frequency = {}
+
+for line in hydrothermal_vent_lines:
+    for pos in get_line_vent_positions(line):
+        if pos in hydrothermal_vents_positions_frequency:
+            hydrothermal_vents_positions_frequency[pos] += 1
+        else:
+            hydrothermal_vents_positions_frequency[pos] = 1
+
+multiple_vents_positions = []
+for pos in hydrothermal_vents_positions_frequency:
+    if hydrothermal_vents_positions_frequency[pos] > 1:
+        multiple_vents_positions.append(pos)
+
+print(len(multiple_vents_positions))
